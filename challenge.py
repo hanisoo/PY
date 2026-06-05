c56668c4352f2ce0532c54fa5c8b97cea84dc83b
hanisoo <76591974+hanisoo@users.noreply.github.com>
2026-06-06 04:22:06 +0900

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -66,3 +66,21 @@ print(f"Your final tax bill is: ${final_tax_amount(tax_amount, tax_credits)}$")
 # final_tax_amount = tax_amount - apply_tax_credits(tax_amount, tax_credits)
 #
 # print(f"Your tax bill is: ${final_tax_amount}")
+
+
+
+# 챌린지 조건
+# get_yearly_revenue (연간 매출 계산)
+# monthly_revenue (월간 매출)를 인수로 받고, 연간 매출(revenue for a year)을 리턴.
+# get_yearly_expenses (연간 비용 계산)
+# monthly_expenses (월간 비용)를 인수로 받고, 연간 비용(expenses for a year)을 리턴.
+# get_tax_amount (세금 계산)
+# profit (이익)을 인수로 받고, tax_amount (세금 금액)를 리턴.
+# apply_tax_credits (세액 공제 적용)
+# tax_amount (세금 금액), tax_credits (세액 공제율)을 인수로 받고, 할인할 금액(amount to discount)을 리턴.
+
+# 요구사항:
+
+# get_tax_amount 함수는 if/else를 사용해야 합니다.
+# 만약 profit이 100,000 초과이면, 세율은 25%가 되어야 합니다.
+# 아닌 경우에는, 세율은 15%가 되어야 합니다.
